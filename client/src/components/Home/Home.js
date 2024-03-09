6f3c023d1a6e11c299e3b9c4ae77dd7cc4effcb6
ShivaPrasad03 <shivacherukunuri@gmail.com>
2024-03-09 21:27:21 +0530

    client added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,10 @@
 import react from "react";
 import logo from "./Asset_1.svg";
+import signup from "./signup.svg";
+import share_idea from './share_idea.svg';
+import find_people from './find_people.svg';
+import build_community from './build_community.svg';
+import { RiRobot2Fill } from "react-icons/ri";
 import "./Home.css";
 
 export default function Home() {
@@ -14,9 +19,33 @@ export default function Home() {
           </div>
         </nav>
         <div className="intro-cont">
-          <div></div>
-          <div></div>
+          <div className="first-child">
+            <h1 className="intro-title">
+              <span className="special-intro"> Find People </span>Having Same
+              Idea As You
+            </h1>
+          </div>
+          <div className="last-child" >
+           
+            <div className="circular-path">
+              <div> <div><img src={signup} alt="svg"/></div>
+              <p>Create an account with Ditto</p></div>
+              <div> <div><img src={share_idea} alt="svg"/></div>
+              <p>Share your idea using Ditto post</p></div>
+              <div> <div><img src={find_people} alt="svg"/></div>
+              <p>find people having same idea</p></div>
+              <div> <div><img src={build_community} alt="svg"/></div>
+              <p>build comunnity & make an impact</p></div>
+             
+            </div>
+            
+
+          </div>
+          
         </div>
+        <div className="bot">
+           <RiRobot2Fill color="white" fontSize={20}/>
+          </div>
       </div>
     </>
   );
